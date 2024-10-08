cda62732cede7a2c809ead9d25cbe9435c089538
bonse koo <bonse88@hs.ac.kr>
2024-10-08 16:17:46 +0900

    first

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { getBoard, getBoardDetail } = require('../controllers/getBoard');
 
 router.get('/get_board', getBoard);
-router.get('/get_board_detail/:id', getBoardDetail);
+router.get('/get_board_user/:id', getBoardDetail);
 
 module.exports = router;
